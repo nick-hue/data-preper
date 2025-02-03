a6ec32f7332268cd8ea080c1f22212c026d2b7d7
nick-hue <nickagelid@gmail.com>
2025-02-03 19:15:01 +0200

    added type checking and reading config from yaml file

--- prep_data.py
+++ prep_data.py
@@ -1,29 +1,44 @@
 import argparse
 from pathlib import Path
 import yaml
-from dataclasses import dataclass
+from dataclasses import dataclass, fields, _MISSING_TYPE
+from typing import Literal, get_type_hints
+import logging
 
 @dataclass
 class Preper:
-    train_method: str 
-    sfm_tool: str
-    matcher_type: str 
+    train_method: Literal["nerfacto", "splatfacto"] = "nerfacto"
+    sfm_tool: Literal["colmap", "glomap"] = "colmap"
+    matching_method: Literal["exhaustive", "sequential", "vocab_tree"] = "vocab_tree"
 
+    def __post_init__(self) -> None:
+        for field in fields(self):
+            field_value = getattr(self, field.name)
+            allowed_values = field.type.__args__
+            if field_value not in allowed_values:
+                raise ValueError(f"Invalid value <{field_value} for field [{field.name}]. Allowed values are: {allowed_values}.")
+
+            if not isinstance(field.default, _MISSING_TYPE) and getattr(self, field.name) is None:
+                raise ValueError(f"No value was passed for field : {field.name}")
 
 
 def read_config_file(config_file: Path):
     with open(config_file, 'r') as f:
         data = yaml.load(f, Loader=yaml.SafeLoader)
     
-    return Preper(*data.values())
+    return Preper(train_method=data['train_method'], sfm_tool=data['sfm_tool'], matching_method=data['matching_method'])
 
-
-def run_sfm(config_file: Path, output_dir: Path):
+def run_sfm(config_file: Path, output_dir: Path, vocab_tree_path: Path):
     # print(f"{config_file=}")
 
     preper: Preper = read_config_file(config_file=config_file)
+
+    if preper.matching_method == "vocab_tree" and vocab_tree_path is None:
+        raise FileNotFoundError("If [matching_method] is <vocab_tree>, then a [vocab_tree_path] is needed.")
+    
     print(preper)
     print(f"{output_dir=}")
+    print(f"{vocab_tree_path=}")
 
 
 
@@ -31,10 +46,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run COLMAP with specified image path and matcher type.")
     parser.add_argument('--config_file', required=True, help="Path to the config file.")
     parser.add_argument('--output_dir', required=True, help="Path to the output directory.")
+    parser.add_argument('--vocab_tree_path', required=False, help="Path to the vocab tree, only needed when <matching_method> is <vocab_tree>.")
     
     args = parser.parse_args()
 
-    run_sfm(args.config_file, args.output_dir)
+    run_sfm(args.config_file, args.output_dir, args.vocab_tree_path)
 
 
 
